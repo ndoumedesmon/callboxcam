c6e0a39bd93f7a1b0484570dc9bee85f81412910
ndoumedesmon <ndoumedesmon@yahoo.com>
2023-10-04 23:59:48 +0100

    fixing the migration code

--- app/Models/cbc_tables.php
+++ app/Models/cbc_tables.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class test extends Model
+class cbc_tables extends Model
 {
     use HasFactory;
 }
